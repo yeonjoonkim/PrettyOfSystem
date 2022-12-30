bd2a0176fd1498228d0554442e9d12d7f33e4c59
yeonjoon <yeonjoon.kim.jwl@gmail.com>
2022-12-30 13:59:04 +1000

    implement firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyD9YERNmCFPATrolEU5icX0s2bSZS03FGc",
+    authDomain: "prettyofsystem.firebaseapp.com",
+    projectId: "prettyofsystem",
+    storageBucket: "prettyofsystem.appspot.com",
+    messagingSenderId: "204754094061",
+    appId: "1:204754094061:web:51aede03733baed57249e4",
+    measurementId: "G-KJVX7Z430Y"
+  },
   production: false
 };
 
