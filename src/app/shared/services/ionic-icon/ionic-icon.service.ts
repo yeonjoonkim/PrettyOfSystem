d46025cdcac23713ee58cc5c88eab9b4183fc869
yeonjoon <yeonjoon.kim.jwl@gmail.com>
2023-02-23 22:49:54 +1000

    set up for the ionic icon list module

--- src/app/shared/services/ionic-icon/ionic-icon.service.ts
+++ src/app/shared/services/ionic-icon/ionic-icon.service.ts
@@ -6,10 +6,14 @@ import ionicIcon from './ionic-icon-list';
 export class IonicIconService {
   constructor() { }
 
+  /**This will return only contains outline icon */
   getOutlineIcon(){
     return ionicIcon.filter(icon => icon.name.endsWith("outline"));
   }
 
-
+  /**This will return only contains sharp icon list. */
+  getSharpIcon(){
+    return ionicIcon.filter(icon => icon.name.endsWith("sharp"));
+  }
 
 }
